cafea785cb17e7b0278b9ec449b64e2311d933ae
Surya Sharma <surya@surya.zone>
2022-06-02 21:55:06 -0500

    moved usernames out into a file

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ RAINBOW_BOUNDARIES = [
     ([120, 50, 20], [135, 255, 255]),  # violet/purple
 ]
 
-USERNAMES = ["exxonmobil", "RogersHelps", "Facebook", "fbsecurity"]
+USERNAMES = []
+with open("usernames.txt", "r") as username_file:
+    for username in username_file:
+        USERNAMES.append(username.strip())
 
 
 def check_image_contains_colours(image_path, colour_boundaries):
@@ -63,6 +66,7 @@ def main():
 
     for username in USERNAMES:
         user = client.get_user(username=username, user_fields="profile_image_url")
+        print(user)
         profile_pic_url = user[0].data["profile_image_url"]
         profile_pic = requests.get(profile_pic_url)
         profile_pic_path = f"profile_pics/{username}_pp_{datetime.utcnow().isoformat()}.png"
